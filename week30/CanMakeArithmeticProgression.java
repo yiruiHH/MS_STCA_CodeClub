44ce994c1202594a52a2a9b459dc0c76e6628e20
yiruiHH <42966395+yiruiHH@users.noreply.github.com>
2020-07-16 19:39:31 +0800

    Create CanMakeArithmeticProgression.java

---
+++ week30/CanMakeArithmeticProgression.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+
+class CanMakeArithmeticProgression {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        for(int i = 2; i < arr.length; i++){
+            if(arr[i] - arr[i - 1] != diff) return false;
+        }
+        return true;
+    }
+}
